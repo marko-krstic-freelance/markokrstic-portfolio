7c3bed978c40f989e2f52cbb601a922d23617b05
Marko Krstic <mailme@markokrstic.com>
2025-04-20 12:38:47 +0200

    ....

---
+++ src/tax-terms-nourl/render.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Tax Terms no URL – hello from a dynamic block!', 'tax-terms-nourl' ); ?>
+</p>
